04038006ccfb4e9647e01b55e71fa601f1bb7e51
zgreathouse <greathouse.zachary@gmail.com>
2018-01-03 18:47:53 -0800

    implement React-Router inside of src/index.js, build PostsIndex component

---
+++ src/components/PostsIndex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>Posts Index</div>
+    )
+  }
+}
+
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+//root reducer
 import reducers from './reducers';
+
+//components
+import PostsIndex from './components/PostsIndex';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
+
+  //<App />
