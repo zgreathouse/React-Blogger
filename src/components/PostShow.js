ccfa396855fb144dcf1a27ea8be1533fa072a764
zgreathouse <greathouse.zachary@gmail.com>
2018-01-05 15:58:56 -0800

    add conditional to PostShow to only render an empty div if the specific post has not been fetched yet, write FETCH_POST case inside of postsReducer

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>Post Show</div>
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
     )
   }
 }
 
-export default PostShow;
+const mapStateToProps = ({ posts }, ownProps) => ({
+  post: posts[ownProps.match.params.id]
+});
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -4,12 +4,12 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 const postsReducer = (state = {}, action) => {
   switch(action.type) {
     case FETCH_POST:
-      const { data } = actions.payload;
+      const { data } = action.payload;
       return { ...state, [data.id]: data };
 
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
-      
+
     default:
       return state;
   }
