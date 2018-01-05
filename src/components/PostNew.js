d0e6dc49c487f3e8c58324d6029ec4d191a95576
zgreathouse <greathouse.zachary@gmail.com>
2018-01-05 14:01:06 -0800

    build out PostNew component, set up error handeling with redux form, dry up render function

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -1,11 +1,73 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+//components
+
+
+const formFields = [
+  {label: 'Title', name: 'title'},
+  {label: 'Categories', name: 'categories'},
+  {label: 'Post Content', name: 'content'}
+];
 
 class PostNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
+  renderFields() {
+    return _.map(formFields, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={this.renderField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    })
+  }
+
   render() {
     return (
-      <div> PostNew! </div>
+      <form>
+        {this.renderFields()}
+      </form>
     )
   }
 }
 
-export default PostNew;
+const validate = values => {
+  const errors = {};
+
+  //validate inputs
+  if (!values.title) {
+    errors.title = "Please enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Please enter at least one category!";
+  }
+  
+  if (!values.content) {
+    errors.content = "Please enter some content!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostNew);
